d54dd404b60ef511ba47f064c0ced5d81368d97d
Htut Khine <htutkhwin@gmail.com>
2019-03-23 05:02:58 +0630

    Finished integrating autocomplete.

---
+++ public/javascripts/autocomplete.js
@@ -0,0 +1,37 @@
+let d = [ 
+    {
+        "name": "Myanmar", 
+        "capital": "yangon",
+        "continent": "asia",
+        "img": "https://dynamic.zacdn.com/d3kfTdUmZH3RnXI82eNpEhpasqc=/fit-in/346x500/filters:quality(95):fill(ffffff)/http://static.my.zalora.net/p/bonia-1586-1393741-1.jpg"
+    }, 
+    {
+        "name": "Morocco", 
+        "capital": "yangon",
+        "continent": "asia",
+        "img": "https://dynamic.zacdn.com/d3kfTdUmZH3RnXI82eNpEhpasqc=/fit-in/346x500/filters:quality(95):fill(ffffff)/http://static.my.zalora.net/p/bonia-1586-1393741-1.jpg"
+    }, 
+    {
+        "name": "Thailand", 
+        "capital": "bangkok",
+        "continent": "asia",
+        "img": "https://dynamic.zacdn.com/d3kfTdUmZH3RnXI82eNpEhpasqc=/fit-in/346x500/filters:quality(95):fill(ffffff)/http://static.my.zalora.net/p/bonia-1586-1393741-1.jpg"
+    }, 
+    {
+        "name": "Singapore", 
+        "capital": "singapore",
+        "continent": "asia",
+        "img": "https://dynamic.zacdn.com/d3kfTdUmZH3RnXI82eNpEhpasqc=/fit-in/346x500/filters:quality(95):fill(ffffff)/http://static.my.zalora.net/p/bonia-1586-1393741-1.jpg"
+    }, 
+];
+
+$('.flexdatalist').flexdatalist({
+    minLength: 1,
+    selectionRequired: true,
+    visibleProperties: ["img", "name", "capital", "continent"],
+    iconProperty: "img",
+    searchIn: 'name',
+    data: d 
+});
+
+$('.item-img').addClass('border border-light');
