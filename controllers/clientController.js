d84f037f078f7ca1d957702fa7307d55590cfdad
kaungmyat <kaungmyathtaywin@gmail.com>
2019-04-06 03:50:00 +0630

    Add fixed add to cart button and Build full shopping page

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,15 +1,16 @@
 const util = require('../util/tools');
 
-exports.home_page = (req, res, next) => { 
-    res.render('home'); 
+exports.home_page = (req, res, next) => {
+    res.render('home');
 }
 
-exports.product_detail = (req, res, next) => { 
-    try { 
+exports.product_detail = (req, res, next) => {
+    try {
         const id = util.convertToObjectId(req.params.id);
-        res.render('product_detail', { data: 'something' }); 
-    }
-    catch(err) { 
+        res.render('productDetail', {
+            data: 'something'
+        });
+    } catch (err) {
         next(err);
     }
 }
